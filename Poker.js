5ee29485588357c61c3c6c203d99e8d35e354334
Jake Cliff <jcliff23@gmail.com>
2019-05-16 15:31:10 -0700

    improveing

--- Poker.js
+++ Poker.js
@@ -1,25 +1,47 @@
 // Import express
 var express = require('express');
-
 // Create an instance of the app
 var app = express();
-
+var players =["0"];
 // Set ejs as the template engine
 app.set("view engine", "ejs");
 
 app.get("/", function(req, resp){
 	
 	// Generate am HTML file from the template
-	// and send it to the client	
-	resp.render("Poker", {occup: "Murka"});
+	// and send it to the client
+	pnum = players.length-1;
+	if(pnum > 1){
+		resp.render("Poker", {occup: pnum, room:"/full"});
+	}else{
+		resp.render("Poker", {occup: pnum, room:"/room"});
+	}
+});
+app.get("/room", function(req, resp){
+	
+	players.push("1");	
+	var cards = deal_cards();
+	resp.render("Room", {c1: cards[0], c2: cards[1], c3: cards[2], round:"1", });
 	
 });
-app.get("/script", function(req, resp){
+app.get("/full", function(req, resp){
 	
-	// Generate am HTML file from the template
-	// and send it to the client	
-	resp.render("Room", {c1: "7", c2: "2", c3: "13", round:"1"});
+	resp.render("Full");
 	
 });
+function compare(a,b)
+{
+    if (a >= b) return true
+    else return false
+}
+function deal_cards()
+{
+    // returns 3 numbers between 1 to 15
+    var hand =  []
+    for (var i = 0; i < 3; i++)
+		hand[i] = ("0" + Math.floor(1 + (Math.random() * 15))).slice(-2);
+    return hand
+}
+
 // Listen for requests
 app.listen(3000);
